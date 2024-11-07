be2e1b0af907110a2814a8144f1e7f1561863e36
rplusq <rafaplusq@gmail.com>
2024-11-07 00:11:33 +0000

    chore: add .solhint ignore

--- test/utils/Merkle.sol
+++ test/utils/Merkle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.25;
 
 /// @notice Nascent, simple, kinda efficient (and improving!) Merkle proof generator and verifier
 /// @author dmfxyz
