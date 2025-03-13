6b8cecff2d955624679201a7c2e6319e7cd09a66
rplusq <rafaplusq@gmail.com>
2025-03-13 04:34:08 +0000

    fixup! feat: l2wct -> 7802 / NTT

--- evm/src/WCT.sol
+++ evm/src/WCT.sol
@@ -16,13 +16,7 @@ import { INttToken } from "src/interfaces/INttToken.sol";
 /// @title WCT Token
 /// @notice This contract implements the L1 WCT token with burn, permit, and voting functionality
 /// @author WalletConnect
-contract WCT is
-    ERC20VotesUpgradeable,
-    ERC20PermitUpgradeable,
-    ERC20BurnableUpgradeable,
-    OwnableUpgradeable,
-    NttTokenUpgradeable
-{
+contract WCT is NttTokenUpgradeable, ERC20VotesUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
     /// @notice Initialization data for the contract
     struct Init {
         /// @dev The address that will be the initial owner of the contract
@@ -39,24 +33,17 @@ contract WCT is
     /// @notice Initializes the WCT token
     /// @param init The initialization data for the contract
     function initialize(Init calldata init) public initializer {
-        __ERC20_init({ name_: "WalletConnect", symbol_: "WCT" });
+        __NttToken_init(init.initialMinter, "WalletConnect", "WCT");
         __ERC20Permit_init("WalletConnect");
         __ERC20Votes_init();
         __ERC20Burnable_init();
         __Ownable_init({ initialOwner: init.initialOwner });
-        __NttToken_init(init.initialMinter);
     }
 
     /// @notice A function to set the new minter for the tokens.
     /// @param newMinter The address to add as both a minter and burner.
     function setMinter(address newMinter) external override onlyOwner {
         _setMinter(newMinter);
-    }
-
-    /// @notice A function that will burn tokens held by the `msg.sender`.
-    /// @param _value The amount of tokens to be burned.
-    function burn(uint256 _value) public override(INttToken, ERC20BurnableUpgradeable) onlyMinter {
-        ERC20BurnableUpgradeable.burn(_value);
     }
 
     /// @notice Returns the current timestamp as a uint48
@@ -70,15 +57,6 @@ contract WCT is
     // solhint-disable-next-line func-name-mixedcase
     function CLOCK_MODE() public pure override returns (string memory) {
         return "mode=timestamp";
-    }
-
-    // Implement the abstract functions from NttTokenUpgradeable
-    function _mint(address account, uint256 amount) internal override(NttTokenUpgradeable, ERC20Upgradeable) {
-        ERC20Upgradeable._mint(account, amount);
-    }
-
-    function _burn(address account, uint256 amount) internal override(NttTokenUpgradeable, ERC20Upgradeable) {
-        ERC20Upgradeable._burn(account, amount);
     }
 
     // The following functions are overrides required by Solidity.
